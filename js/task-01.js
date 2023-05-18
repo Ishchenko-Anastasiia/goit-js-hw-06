f4c65da17711d069d452f13bb315b133253864c3
Ishchenko-Anastasiia <123247356+Ishchenko-Anastasiia@users.noreply.github.com>
2023-05-18 14:44:52 +0300

    hw 6.2

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const items = document.querySelectorAll("#categories .item");
 
 console.log(`Number of categories: ${items.length}`);
 
-categoriesList.forEach(item => {
+items.forEach(item => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
     console.log(`Elements: ${item.lastElementChild.children.length}`)
 });
